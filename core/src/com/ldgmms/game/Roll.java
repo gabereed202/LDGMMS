2dfb904c8915cc7d5e6c2caee8130d401c4d92bc
sneaky <sean.m.kennedy@wsu.edu>
2022-02-19 02:03:07 -0800

    Basic movement working. Added a simple Player and a few Screens to start building off of. Hex movement/collision needs work.

---
+++ core/src/com/ldgmms/game/Roll.java
@@ -0,0 +1,14 @@
+package com.ldgmms.game;
+import com.badlogic.gdx.math.MathUtils;
+
+
+public class Roll {
+    public static int dice(int numDice, int sides){ //will let you define any sided die (do 100 sides for percentile)
+        int sum = 0;
+        for (int i = 0; i < numDice; i++){
+            sum += MathUtils.random(1, sides);
+        }
+        return sum;
+
+    }
+}
---
+++ desktop/build/resources/main/map_hexTileset.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="map_hexTileset" tilewidth="32" tileheight="32" tilecount="4" columns="4">
+ <image source="map_hexTilesheet.png" width="128" height="32"/>
+ <tile id="3">
+  <properties>
+   <property name="blocked" type="bool" value="true"/>
+  </properties>
+ </tile>
+</tileset>
