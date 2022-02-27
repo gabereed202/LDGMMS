fef9b3a6dfc91ab043d4b0905738fc8852a10290
Matthew Rease <scubaventure101@hotmail.com>
2022-02-27 15:31:31 -0800

    Implement libgdx tile interface.

--- core/src/com/ldgmms/game/Tile.java
+++ core/src/com/ldgmms/game/Tile.java
@@ -1,6 +1,10 @@
 package com.ldgmms.game;
 
 import java.util.ArrayList;
+import com.badlogic.gdx.maps.tiled.TiledMapTile;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.maps.MapProperties;
+import com.badlogic.gdx.maps.MapObjects;
 
 /**
  * A single tile on a game map.
@@ -8,10 +12,112 @@ import java.util.ArrayList;
  *   need to treat the neighboring tiles slightly differently.
  * @author Matthew Rease
  */
-public class Tile {
+public class Tile implements TiledMapTile {
+	// Interface fields
+	private int id;
+	private TiledMapTile.BlendMode blend_mode;
+	private TextureRegion texreg;
+	private float off_x, off_y;
+	private MapProperties props;
+	private MapObjects objs;
+
+	// Class fields
 	private TileType tileId;
 	private Tile neighbor[]; // Represents 3x3 grid of tiles, with the middle being `this`. Should be of length 8...
 	GenericUnit unit = null;
+
+	// Interface methods
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	/**
+	 *
+	 * @return the {@link TiledMapTile.BlendMode} to use for rendering this tile
+	 */
+	public TiledMapTile.BlendMode getBlendMode() {
+		return blend_mode;
+	}
+
+	/**
+	 * Sets the {@link TiledMapTile.BlendMode} to use for rendering this tile
+	 * @param blendMode the blend mode to use for rendering the tile
+	 */
+	public void setBlendMode(TiledMapTile.BlendMode blendMode) {
+		blend_mode = blendMode;
+	}
+
+	/**
+	 *
+	 * @return texture region used to render the tile
+	 */
+	public TextureRegion getTextureRegion() {
+		return texreg;
+	}
+
+	/**
+	 *
+	 * @param textureRegion
+	 * @return Sets the texture region used to render the tile
+	 */
+	public void setTextureRegion(TextureRegion textureRegion) {
+		texreg = textureRegion;
+	}
+
+	/**
+	 *
+	 * @return the amount to offset the x position when rendering the tile
+	 */
+	public float getOffsetX() {
+		return off_x;
+	}
+
+	/**
+	 * Set the amount to offset the x position when rendering the tile
+	 * @param offsetX
+	 */
+	public void setOffsetX(float offsetX) {
+		off_x = offsetX;
+	}
+
+	/**
+	 *
+	 * @return the amount to offset the y position when rendering the tile
+	 */
+	public float getOffsetY() {
+		return off_y;
+	}
+
+	/**
+	 * Set the amount to offset the y position when rendering the tile
+	 * @param offsetY
+	 */
+	public void setOffsetY(float offsetY) {
+		off_y = offsetY;
+	}
+
+	/**
+	 *
+	 * @return tile's properties set
+	 */
+	public MapProperties getProperties() {
+		return props;
+	}
+
+	/**
+	 *
+	 * @return collection of objects contained in the tile
+	 */
+	public MapObjects getObjects() {
+		return objs;
+	}
+
+	// Class methods
 
 	/**
 	 * Value used for things such as tile drawing.
