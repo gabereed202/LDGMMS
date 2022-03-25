e89eea3ddc800422bbedf6f44bc89a544a855f52
sneaky <sean.m.kennedy@wsu.edu>
2022-03-24 18:25:08 -0700

    Added a healthbar to the top left corner of the SquareScreen that changes colors based on current player HP. Can be tested with spacebar. Added some more tests.

--- core/src/com/ldgmms/game/PlayerTest.java
+++ core/src/com/ldgmms/game/PlayerTest.java
@@ -7,6 +7,9 @@ import static org.junit.Assert.*;
 
 public class PlayerTest {
 
+    /**
+     * Acceptance test.
+     */
     @Test
     public void playerNotNullIfExists() {
         // should pass
@@ -14,6 +17,9 @@ public class PlayerTest {
         assertNotNull(player);
     }
 
+    /**
+     * Acceptance test.
+     */
     @Test
     public void playerNullIfExists() {
         // should fail
@@ -21,20 +27,74 @@ public class PlayerTest {
         assertNull(player);
     }
 
+    /**
+     * Integration test: bottom-up approach.
+     * Tests the view component (health color) and the model component (player's health).
+     */
     @Test
     public void testPlayerFullHealthAndColorIsGreen() {
         // should pass
         Player player = new Player();
+        assertTrue(player.getHP() == 100);
         assertTrue(player.getHealthColor().equals(HealthColor.GREEN));
     }
 
+    /**
+     * Integration test: bottom-up approach.
+     * Tests the view component (health color) and the model component (player's health).
+     */
     @Test
     public void testPlayerFullHealthAndColorNotGreen() {
         // should pass
         Player player = new Player();
+        assertTrue(player.getHP() == 100);
         assertFalse(!player.getHealthColor().equals(HealthColor.GREEN));
     }
 
+    /**
+     * Acceptance test.
+     */
+    @Test
+    public void testPlayerHealthIs0AndPlayerIsDead() {
+        Player player = new Player();
+        player.setHP(0);
+        assertTrue(!player.isAlive());
+    }
+
+    /**
+     * Acceptance test.
+     */
+    @Test
+    public void testPlayerHealthIs0AndPlayerIsNotDead() {
+        Player player = new Player();
+        player.setHP(0);
+        assertFalse(player.isAlive());
+    }
+
+    /**
+     * The rest of the tests make up White-box tests which tests the view portion of the player's health.
+     *
+     * Below is the method being tested:
+     * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+     * public void setHP(int hp) {
+     *         this.hp = hp;
+     *
+     *         if (hp == 0) {
+     *             setAlive(false);
+     *         }
+     *
+     *         if (hp >= 75) {
+     *             setHealthColor(HealthColor.GREEN);
+     *         } else if (hp < 75 && hp >= 50) {
+     *             setHealthColor(HealthColor.YELLOW);
+     *         } else if (hp < 50 && hp >= 25) {
+     *             setHealthColor(HealthColor.ORANGE);
+     *         } else if (hp < 25) {
+     *             setHealthColor(HealthColor.RED);
+     *         }
+     *     }
+     * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+     */
     @Test
     public void testPlayerHealthBetween50And75AndColorIsYellow() {
         // should pass
@@ -82,18 +142,7 @@ public class PlayerTest {
         player.setHP(1);
         assertFalse(!player.getHealthColor().equals(HealthColor.RED));
     }
-
-    @Test
-    public void testPlayerHealthIs0AndPlayerIsDead() {
-        Player player = new Player();
-        player.setHP(0);
-        assertTrue(!player.isAlive());
-    }
-
-    @Test
-    public void testPlayerHealthIs0AndPlayerIsNotDead() {
-        Player player = new Player();
-        player.setHP(0);
-        assertFalse(player.isAlive());
-    }
+    /**
+     * End of White-box testing
+     */
 }
